9eaee97dd465607ff12dcee2752b6ef956e43e0e
Ammar Al-khawaldeh <3mmar.g97@gmail.com>
2016-12-26 22:30:51 +0200

    Cleaning code

--- includes/form.php
+++ includes/form.php
@@ -1,15 +1,15 @@
 <?php
 	if (isset($_GET['result'])):?>
-		<input type="text" class="copyText" value="<?=str_replace('index.php?result','?u',
-			'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']) ?>" readonly>
+		<input type="text" class="copyText" value="<?= str_replace('index.php?result', '?u',
+			'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) ?>" readonly>
 		<input type="button" class="copyBtn" value="Copy url">
 		<input type="button" onclick="home()" value="Return to home">
 		<?php
 	elseif (isset($_GET['err'])):?>
-		<input type="text" value="Please enter a valid URL ." style="color: #f00;" readonly>
+		<input type="text" value="<?=$_GET['err']?>" style="color: #f00;" readonly>
 		<input type="button" onclick="home()" value="Return to home">
 	<?php else: ?>
-		<form action="process.php" method="get">
+		<form method="POST">
 			<input type="text" name="url" placeholder="Insert your URL">
 			<input type="submit" value="Get short URL">
 		</form>
--- themes/shortcut2016/index.php
+++ themes/shortcut2016/index.php
@@ -5,61 +5,19 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
 	</head>
-<!--TODO: create functions to get all informations for the themes-->
+	<!--TODO: create functions to get all informations for the themes-->
 	<body>
 		<div class="body">
 
 			<div class="outer">
 
 				<div class="middle">
-					<!--NavBar Start-->
-					<div class="navBar">
-						<div class="navContainer">
-							<!-- TODO: it will convert to user photo when user is signed in . -->
-							<img class='logo' src="themes/shortcut2016/images/logo.png"/>
-							<div class="line"></div>
-
-							<!-- TODO: it will convert to user name when user is signed in . -->
-							<span class="name"><h4>WebSite Name</h4></span>
-
-							<div class="navOtherSide">
-								<button type="button" class="inButton"><i class="fa fa-sign-in" aria-hidden="true"></i>
-									&nbsp;Sign in
-								</button>
-								<button type="button" class="UpButton"><i class="fa fa-user-plus"
-								                                          aria-hidden="true"></i>
-									&nbsp;Sign up
-								</button>
-
-
-								<div class="menuBtn" onclick="openNav()">
-									<i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-								</div>
-
-
-							</div>
-
-
-						</div>
-					</div>
-					<!--NavBar End-->
-					<div class="header">
-						<!--TODO: let user choose website name-->
-						<div class="WebSiteName">
-							<h1>WebSite Name</h1>
-						</div>
-
-						<!--TODO: let user choose website description-->
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
-					</div>
+					<?php include 'includes/nav.php';
+						include 'includes/header.php' ?>
 					<div class="inner">
 						<?php include 'includes/form.php' ?>
 					</div>
-					<div class="footer">
-						<p>Copyright (c) 2016 Copyright Holder All Rights Reserved.</p>
-					</div>
-
-
+					<?php include 'includes/footer.php' ?>
 				</div>
 
 
